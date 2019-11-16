5664498261b88375647ec297915ddda465dc8059
leomirco <leomirco@gmail.com>
2019-11-16 17:59:32 +0100

    Update miatemp.js

--- miatemp.js
+++ miatemp.js
@@ -1,10 +1,10 @@
-function hello(req, res) {
-  var name = req.swagger.params.name.value || 'stranger';
-  var hello = util.format('Hello, %s!', name);
-  
-  // Add this log statement to see calls
-  console.log('=== NodeMCU ' + name + ' ===');
-  
-  // this sends back a JSON response string
-  res.json(hello);
-}
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
